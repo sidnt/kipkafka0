67e9b52de08728d3090b3f3c5d7e783b6c589b37
a28365 <a28365@mbp>
2018-08-23 23:08:09 +0530

    working

--- src/main/scala/kafkaclient/StudentConsumer.scala
+++ src/main/scala/kafkaclient/StudentConsumer.scala
@@ -19,18 +19,19 @@ object StudentConsumer extends App {
   props.put("bootstrap.servers", "localhost:9092")
 
   props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
-  props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
-  // props.put("value.deserializer", "kafkaclient.StudentSerializer")
+  // props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put("value.deserializer", "kafkaclient.StudentDeserializer")
   props.put("group.id", "studentconsumers")
 
-  val consumer = new KafkaConsumer[String, String](props)
+  val consumer = new KafkaConsumer[String, Student](props)
 
   consumer.subscribe(util.Collections.singletonList(TOPIC))
 
-  while(true){
-    val records: ConsumerRecords[String,String] = consumer.poll(100)
+  // while(true){
+    val records: ConsumerRecords[String,Student] = consumer.poll(5000)
     for (record<-records.asScala){
+      println("ab print kar raha")
      println(record.value())
     }
-  }
+  // }
 }
