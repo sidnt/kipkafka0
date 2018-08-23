6a3aa7212f59f17ff6f014d7a38fbd655951e666
a28365 <a28365@mbp>
2018-08-23 17:57:12 +0530

    custom deserializer, compiling

--- src/main/scala/kafkaclient/Serialization.scala
+++ src/main/scala/kafkaclient/Serialization.scala
@@ -22,6 +22,24 @@ class StudentSerializer extends Serializer[Student] {
 
 }
 
-// class StudentDeserializer extends Deserializer[Student] {
+class StudentDeserializer extends Deserializer[Student] {
 
-// }
+  def close(): Unit = {}
+  def configure(configs: java.util.Map[String, _], isKey: Boolean): Unit = {}
+
+  def deserialize(topic: String, bytes: Array[Byte]): Student = {
+
+    try{
+      val stringRep = bytes.map(_.toChar).foldLeft("")(_+_) //eg "Student(1,Ayush)"
+      val arr = stringRep split ","
+      val (preId, preName) = (arr(0),arr(1))
+      val (id,name) = (preId.drop(8).toInt, preName.init)
+      Student(id,name)
+    }
+    catch {
+      case ex: Exception => throw new Exception(ex.getMessage)
+    }
+    
+  }
+
+}
